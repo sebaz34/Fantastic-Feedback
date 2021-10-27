e41e14f517b7714ff38aa7f42d571f52c7692662
Sebastian Vowels <sebastian.vowels@outlook.com>
2021-10-27 13:49:01 +1000

    WEB503 Submission
    Submission to WEB503

--- Code/Front End/FFFrontEnd/FFFrontEnd/Startup.cs
+++ Code/Front End/FFFrontEnd/FFFrontEnd/Startup.cs
@@ -43,16 +43,16 @@ namespace FFFrontEnd
         {
             app.UseDeveloperExceptionPage();
 
-            //if (env.IsDevelopment())
-            //{
-                
-            //}
-            //else
-            //{
-            //    app.UseExceptionHandler("/Home/Error");
-            //    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            //    app.UseHsts();
-            //}
+            if (env.IsDevelopment())
+            {
+
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
